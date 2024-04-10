b0ade5ca9f2ffcdd09820f13aaf8ae88e5fc0f5a
tsingloong611 <tsingloong611@gmail.com>
2024-04-10 21:00:29 +0800

    2

---
+++ kum-mod-template-1.20.4/src/client/java/com/kum/KumModClient.java
@@ -0,0 +1,10 @@
+package com.kum;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class KumModClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
